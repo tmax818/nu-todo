76d768273241de97a5095ccfe7ef718f63b7b88a
tmax818 <tylermaxwell661@gmail.com>
2020-11-21 20:14:19 -0800

    init commit

---
+++ src/shared/items.js
@@ -0,0 +1,44 @@
+export const items = [
+    {
+        id: 0,
+        name: "Laundry",
+        image: "assets/images/laundry.jpg",
+        description: "Do the laundry at the Coin 'n Suds!",
+        completed: false
+    },
+    {
+        id: 1,
+        name: "Walk Dog",
+        image: "assets/images/dog.jpg",
+        description: "Take Sparky for a walk.",
+        completed: false
+    },
+    {
+        id: 2,
+        name: "Create 'to-do' App",
+        image: "assets/images/todo.jpg",
+        description: "Finish this damn app!",
+        completed: false
+    },
+    {
+        id: 3,
+        name: "Buy Groceries",
+        image: "assets/images/grocery.jpg",
+        description: "Go to the store! We need food!",
+        completed: false
+    },
+    {
+        id: 4,
+        name: "Study React",
+        image: "assets/images/react.jpg",
+        description: "React is hard!",
+        completed: false
+    },
+    {
+        id: 5,
+        name: "Exercise",
+        image: "assets/images/exercise.jpg",
+        description: "Too much coding makes your butt look big!",
+        completed: false
+    },
+]
