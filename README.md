97668dd0c952133c3a7b36ef42fb4a7502c3431e
tmax818 <tylermaxwell661@gmail.com>
2020-11-23 14:51:25 -0800

    add state to App.js

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Sample project for React
 
 # week 2
+
+- Add state to [App.js](src/App.js)
--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import './App.css';
+import {items} from "./shared/items"
 
 class App extends Component {
+
+  state = {items: items}
   render() {
+    console.log(this.state)
     return (
       <div>
         App
