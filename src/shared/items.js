91bdcdda555d7f9eee58bb0fcc9f676609653f40
tmax818 <tylermaxwell661@gmail.com>
2020-11-22 21:05:00 -0800

    add notes

--- src/shared/items.js
+++ src/shared/items.js
@@ -4,41 +4,60 @@ export const items = [
         name: "Laundry",
         image: "assets/images/laundry.jpg",
         description: "Do the laundry at the Coin 'n Suds!",
-        completed: false
+        completed: false,
+        notes: [
+        {
+        id: 0,
+        rating: 5,
+        text: "Don't forget to separate the light and dark, Dummy!",
+        date: "2020-10-25T16:30Z",
+        },
+        {
+        id: 1,
+        rating: 5,
+        text: "Don't forget the quarters!",
+        date: "2020-11-25T16:30Z",
+        }
+    ]
     },
     {
         id: 1,
         name: "Walk Dog",
         image: "assets/images/dog.jpg",
         description: "Take Sparky for a walk.",
-        completed: false
+        completed: false,
+        notes: []
     },
     {
         id: 2,
         name: "Create 'to-do' App",
         image: "assets/images/todo.jpg",
         description: "Finish this damn app!",
-        completed: false
+        completed: false,
+        notes: []
     },
     {
         id: 3,
         name: "Buy Groceries",
         image: "assets/images/grocery.jpg",
         description: "Go to the store! We need food!",
-        completed: false
+        completed: false,
+        notes: []
     },
     {
         id: 4,
         name: "Study React",
         image: "assets/images/react.jpg",
         description: "React is hard!",
-        completed: false
+        completed: false,
+        notes: []
     },
     {
         id: 5,
         name: "Exercise",
         image: "assets/images/exercise.jpg",
         description: "Too much coding makes your butt look big!",
-        completed: false
+        completed: false,
+        notes: []
     },
 ]
