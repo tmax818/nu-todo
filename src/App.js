139ab6fccc8d6a6f9b06f8a3b14f7380281e151c
tmax818 <tylermaxwell661@gmail.com>
2020-11-23 15:46:44 -0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import {Navbar, NavbarBrand} from 'reactstrap'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
+   <div className="App">
+        <Navbar dark color="danger">
+          <div className="container">
+            <NavbarBrand href="/">To-Do</NavbarBrand>
+          </div>
+        </Navbar>
+   
       </div>
     )
   }
