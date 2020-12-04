f31ca99c93211f524d154c828d3ad2759368c45a
tmax818 <tylermaxwell661@gmail.com>
2020-12-03 19:21:24 -0800

    fix

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import {Card, CardImg} from 'reactstrap'
 
-const Item = ({item, onSelect}) => {
- 
+class Item extends Component {
+    render() {
         return (
-            <div key={item.id} className="col-md-3 m-1">
-            <Card onClick={() => onSelect(item)}>
-                <CardImg width="100%" height="25%" src={item.image}/>
-                <h1>{item.name}</h1>
-        <p>{item.description}</p>
+            <div key={this.props.item.id} className="col-md-5 m-1">
+            <Card onClick={() => this.props.onSelect(this.props.item)}>
+                <CardImg width="100%" height="25%" src={this.props.item.image}/>
+                <h1>{this.props.item.name}</h1>
+        <p>{this.props.item.description}</p>
             </Card>
             </div>
         )
-    
+    }
 }
 export default Item
